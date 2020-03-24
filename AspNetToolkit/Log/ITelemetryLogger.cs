65bcdf07df3e8cd3420caa1969ac32605a38cd3d
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-03-25 10:30:36 +1100

    Add Flush

--- AspNetToolkit/Log/ITelemetryLogger.cs
+++ AspNetToolkit/Log/ITelemetryLogger.cs
@@ -4,5 +4,6 @@ namespace AspNetToolkit.Log {
 	public interface ITelemetryLogger {
 		TrackDependencyHandler TrackDependency(string dependencyTypeName, string dependencyName, string command);
 		void TrackException(Exception exception);
+		void Flush();
 	}
 }
