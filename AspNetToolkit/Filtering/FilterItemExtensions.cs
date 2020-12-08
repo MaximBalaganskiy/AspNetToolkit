6d3be5d332763f0e7a0e6254192d308445f61816
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-12-08 12:15:28 +1100

    OR Is and Like filter items

--- AspNetToolkit/Filtering/FilterItemExtensions.cs
+++ AspNetToolkit/Filtering/FilterItemExtensions.cs
@@ -9,14 +9,27 @@ namespace AspNetToolkit.Filtering {
 	public static class FilterItemExtensions {
 		public static Expression<Func<T, bool>> GetPredicate<T>(this List<FilterItem> filterItems, IEnumerable<IFilterItemStrategy<T>> strategies) {
 			Expression<Func<T, bool>> predicate = null;
-			foreach (var fig in filterItems.GroupBy(x => x.Name)) {
+
+			// filter items with the same Guid and operators Equals or Like should be ORed instead of ANDed
+			// this will allow searches similar to "Parameter Name like Crypt OR Parameter Name like Giard"
+			var groups = from filterItem in filterItems
+						 group filterItem by new {
+							 filterItem.Name,
+							 IsOred = filterItem.Operator == FilterOperator.Is || filterItem.Operator == FilterOperator.Like
+						 };
+
+			foreach (var fig in groups) {
 				Expression<Func<T, bool>> groupPredicate = null;
-				var strategy = strategies.FirstOrDefault(x => x.GetType().Name.Replace("FilterItemStrategy", "") == fig.Key);
+				var strategy = strategies.FirstOrDefault(x => x.GetType().Name.Replace("FilterItemStrategy", "") == fig.Key.Name);
 				if (strategy != null) {
 					foreach (var fi in fig) {
 						var fiPredicate = strategy.GetPredicate(fi);
 						if (fiPredicate != null) {
-							groupPredicate = groupPredicate == null ? fiPredicate : groupPredicate.Or(fiPredicate);
+							groupPredicate = groupPredicate == null
+								? fiPredicate
+								: (fig.Key.IsOred
+									? groupPredicate.Or(fiPredicate)
+									: groupPredicate.And(fiPredicate));
 						}
 					}
 					if (groupPredicate != null) {
