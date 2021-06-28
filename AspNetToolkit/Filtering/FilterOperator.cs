daa6582b00b8f9f94233288ccb644c5a9c3b2e71
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-06-28 11:09:57 +1000

    Add more operators

--- AspNetToolkit/Filtering/FilterOperator.cs
+++ AspNetToolkit/Filtering/FilterOperator.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace AspNetToolkit.Filtering {
 	public enum FilterOperator {
-		Like,
-		NotLike,
-		Is,
-		IsNot,
-		LessThan,
-		GreaterThan,
-		IsBefore,
+		Like = 0,
+		NotLike = 1,
+		Is = 2,
+		IsNot = 3,
+		LessThan = 4,
+		GreaterThan = 5,
+		IsBefore = 6,
 		IsAfter,
 		Between
+		IsAfter = 7,
+		IsOnOrAfter = 8,
+		IsBeforeOrOn = 9,
+		LessThanOrEqual = 10,
+		GreaterThanOrEqual = 11
 	}
 }
